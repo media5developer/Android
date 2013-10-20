4c50569b78247ca1d85d4ae70c74722c2d94bc71
media5developer <hiromi.kimura@media5.co.jp>
2013-10-20 23:03:15 +0900

    Update MainActivity.java

--- src/jp/co/media5/android/MainActivity.java
+++ src/jp/co/media5/android/MainActivity.java
@@ -1,15 +1,18 @@
 package jp.co.media5.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		TextView display = new TextView(this);
+        	display.setText("Hello World!!");
+        	setContentView(display);
 	}
 
 	@Override
