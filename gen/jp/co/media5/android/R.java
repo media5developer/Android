6c09627c705c68a91a8b733202121f946ced3e62
satoshiyogoumedia5cojp <satoshi.yogou@media5.co.jp>
2013-11-18 02:41:08 +0900

    自動生成されるファイルをバージョン管理対象から除外

--- gen/jp/co/media5/android/R.java
+++ gen/jp/co/media5/android/R.java
@@ -8,61 +8,110 @@
 package jp.co.media5.android;
 
 public final class R {
+    public static final class array {
+        public static final int calc_messages_equal=0x7f070000;
+    }
     public static final class attr {
     }
-    public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
-         Customize dimensions originally defined in res/values/dimens.xml (such as
-         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
-    
+    public static final class color {
+        public static final int calc_button_text=0x7f040002;
+        /**  Yさん担当画面の色 
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int calc_display_bg=0x7f040000;
+        public static final int calc_display_text=0x7f040001;
+    }
+    public static final class dimen {
+        public static final int calc_button_margin=0x7f050005;
+        public static final int calc_button_padding=0x7f050004;
+        public static final int calc_button_text=0x7f050003;
+        public static final int calc_display_margin=0x7f050002;
+        public static final int calc_display_padding=0x7f050001;
+        /**  Yさん担当画面のサイズ 
+         */
+        public static final int calc_display_text=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int boot_button_i=0x7f090000;
+        public static final int boot_button_k=0x7f090001;
+        public static final int boot_button_y=0x7f090002;
+        public static final int calc_button_add=0x7f090011;
+        public static final int calc_button_divide=0x7f09000e;
+        public static final int calc_button_eight=0x7f09000b;
+        public static final int calc_button_equal=0x7f090013;
+        public static final int calc_button_five=0x7f090008;
+        public static final int calc_button_four=0x7f090007;
+        public static final int calc_button_multiply=0x7f09000f;
+        public static final int calc_button_nine=0x7f09000c;
+        public static final int calc_button_one=0x7f090004;
+        public static final int calc_button_point=0x7f090012;
+        public static final int calc_button_seven=0x7f09000a;
+        public static final int calc_button_six=0x7f090009;
+        public static final int calc_button_subtract=0x7f090010;
+        public static final int calc_button_three=0x7f090006;
+        public static final int calc_button_two=0x7f090005;
+        public static final int calc_button_zero=0x7f09000d;
+        public static final int calc_display=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-    }
-    public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int activity_y=0x7f030001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int activity_name_i=0x7f060002;
+        public static final int activity_name_k=0x7f060003;
+        /**  画面名
+ 画面名
+         */
+        public static final int activity_name_main=0x7f060001;
+        public static final int activity_name_y=0x7f060004;
+        /**  共通 
+ 共通 
+         */
+        public static final int app_name=0x7f060000;
+        public static final int calc_button_add=0x7f060010;
+        public static final int calc_button_divide=0x7f060013;
+        public static final int calc_button_eight=0x7f06000d;
+        public static final int calc_button_equal=0x7f060014;
+        public static final int calc_button_five=0x7f06000a;
+        public static final int calc_button_four=0x7f060009;
+        public static final int calc_button_multiply=0x7f060012;
+        public static final int calc_button_nine=0x7f06000e;
+        public static final int calc_button_one=0x7f060006;
+        public static final int calc_button_point=0x7f06000f;
+        public static final int calc_button_seven=0x7f06000c;
+        public static final int calc_button_six=0x7f06000b;
+        public static final int calc_button_subtract=0x7f060011;
+        public static final int calc_button_three=0x7f060008;
+        public static final int calc_button_two=0x7f060007;
+        /**  Yさん担当画面
+ Yさん担当画面
+         */
+        public static final int calc_button_zero=0x7f060005;
+        public static final int calc_error_dialog_button=0x7f060025;
+        public static final int calc_message_add=0x7f060021;
+        public static final int calc_message_default=0x7f060015;
+        public static final int calc_message_divide=0x7f060024;
+        public static final int calc_message_eight=0x7f06001e;
+        public static final int calc_message_five=0x7f06001b;
+        public static final int calc_message_four=0x7f06001a;
+        public static final int calc_message_multiply=0x7f060023;
+        public static final int calc_message_nine=0x7f06001f;
+        public static final int calc_message_one=0x7f060017;
+        public static final int calc_message_point=0x7f060020;
+        public static final int calc_message_seven=0x7f06001d;
+        public static final int calc_message_six=0x7f06001c;
+        public static final int calc_message_subtract=0x7f060022;
+        public static final int calc_message_three=0x7f060019;
+        public static final int calc_message_two=0x7f060018;
+        public static final int calc_message_zero=0x7f060016;
     }
     public static final class style {
-        /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
-    
- API 11 theme customizations can go here. 
-
-        Base application theme for API 14+. This theme completely replaces
-        AppBaseTheme from BOTH res/values/styles.xml and
-        res/values-v11/styles.xml on API 14+ devices.
-    
- API 14 theme customizations can go here. 
+        public static final int calc_button=0x7f080001;
+        /**  Yさん担当画面のスタイル 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int calc_display=0x7f080000;
     }
 }
